31c82974ba88c9105b9447ea9ded5d022f7a0990
Yinshen Tang <tomtang0514@gmail.com>
2013-04-20 23:07:26 -0500

    UI update 1

--- script/page_navigate.js
+++ script/page_navigate.js
@@ -7,3 +7,17 @@ function openProfilePage(){
     document.getElementById("profile-page").style.display = "block";
     document.getElementById("groups-page").style.display = "none";
 }
+
+function openGroupInfoPage(){
+    document.getElementById("group-info-page").style.display = "block";
+    document.getElementById("chat-page").style.display = "none";
+}
+
+function openChatPage(){
+    document.getElementById("chat-page").style.display = "block";
+    document.getElementById("group-info-page").style.display = "none";
+}
+
+function openPopupBox(){
+    $("#popup-box").fadeToggle(200);
+}
